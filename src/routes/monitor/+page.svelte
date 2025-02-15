4d787793f5f6db2f5deb8ebca5f2b1d2a5c9e94e
Mohsin Firoz <44004341+MohsinFiroz@users.noreply.github.com>
2025-02-15 18:18:40 +0000

    fix

--- src/routes/monitor/+page.svelte
+++ src/routes/monitor/+page.svelte
@@ -2,7 +2,7 @@
     let pageContent = "";
   
     async function fetchPage() {
-      const res = await fetch("https://real-estate-management:8080/v1/metrics");
+      const res = await fetch("https://real-estate-management/v1/metrics");
       pageContent = await res.text();
     }
   
