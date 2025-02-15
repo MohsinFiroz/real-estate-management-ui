b0aca754f78bc8f6de56b9173435ab4b34291892
Mohsin Firoz <44004341+MohsinFiroz@users.noreply.github.com>
2025-02-15 18:36:04 +0000

    fix

--- src/routes/monitor/+page.svelte
+++ src/routes/monitor/+page.svelte
@@ -1,11 +1,21 @@
-<script>
+<script lang="ts">
     let pageContent = "";
-  
+    
     async function fetchPage() {
-      const res = await fetch("https://real-estate-management:80/v1/metrics");
-      pageContent = await res.text();
+      try {
+        const res = await fetch("https://real-estate-management:80/v1/metrics");
+        
+        if (!res.ok) {
+          throw new Error(`Failed to fetch page, status: ${res.status}`);
+        }
+        
+        pageContent = await res.text();
+      } catch (error) {
+        console.error("Error fetching page:", error);
+        throw error; // Rethrow the error after logging it
+      }
     }
-  
+    
     fetchPage();
   </script>
   
