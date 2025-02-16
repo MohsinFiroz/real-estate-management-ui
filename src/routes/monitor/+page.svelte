5ed76af956501fcc7844e556adc20ed88c0c9543
Mohsin Firoz <44004341+MohsinFiroz@users.noreply.github.com>
2025-02-16 05:14:54 +0000

    fix

--- src/routes/monitor/+page.svelte
+++ src/routes/monitor/+page.svelte
@@ -3,7 +3,7 @@
     
     async function fetchPage() {
       try {
-        const res = await fetch("/v1/metrics");
+        const res = await fetch("/_api/v1/metrics");
         
         if (!res.ok) {
           throw new Error(`Failed to fetch page, status: ${res.status}`);
