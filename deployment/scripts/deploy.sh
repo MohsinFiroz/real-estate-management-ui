8c6fc3c61bb56f9e6bb5ef84764797d452434313
Mohsin Firoz <44004341+MohsinFiroz@users.noreply.github.com>
2025-02-16 05:45:59 +0000

    fix it

--- deployment/scripts/deploy.sh
+++ deployment/scripts/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 GITHUB_TOKEN=$1
 GITHUB_ACTOR=$2
 DOCKER_SERVER_IP=$3
@@ -9,17 +10,22 @@ IMAGE_NAME=$(echo "$6" | tr '[:upper:]' '[:lower:]')
 # Log in to GitHub Container Registry
 echo "$GITHUB_TOKEN" | docker login ghcr.io -u "$GITHUB_ACTOR" --password-stdin
 
-# Pull the latest image from Docker Hub
+# Pull the latest image
 echo "Pulling the latest Docker image..."
 docker pull $IMAGE_NAME
 
-# Stop and remove any running container with the same name
+# Stop and remove existing container if running
 echo "Stopping existing Docker container..."
 docker stop real-estate-management-ui || true
 docker rm real-estate-management-ui || true
 
-# Run the new Docker container
+# Run the new container with auto-restart policy
 echo "Running the new Docker container..."
-docker run -d --name real-estate-management-ui --network app-network -p 3000:3000 $IMAGE_NAME
+docker run -d \
+  --name real-estate-management-ui \
+  --network app-network \
+  --restart unless-stopped \
+  -p 3000:3000 \
+  $IMAGE_NAME
 
 echo "Deployment completed successfully!"
