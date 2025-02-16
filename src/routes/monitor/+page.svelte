c1878f3eb9b14857d5c76ab36fb9f994b78508cd
Mohsin Firoz <44004341+MohsinFiroz@users.noreply.github.com>
2025-02-16 06:41:59 +0000

    fix

--- src/routes/monitor/+page.svelte
+++ src/routes/monitor/+page.svelte
@@ -3,7 +3,7 @@
   const iframeUrl = "https://real-estate-management.softcelia.com/v1/metrics";
 </script>
 
-<div class="flex justify-center items-center flex-1 w-full">
+<div class="flex justify-center items-center flex-1 w-full h-full">
   <!-- Direct URL for iframe -->
-  <iframe src={iframeUrl} class="w-full h-full border-none" title="Metrics Page"></iframe>
+  <iframe src={iframeUrl} title="Metrics Page"></iframe>
 </div>
